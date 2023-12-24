dcbf896a449230af93636def70eb1dae7a2666e6
Jack Hsiao <chhsiao@gms.tcu.edu.tw>
2023-12-24 09:12:55 +0800

    Add files via upload

---
+++ Validator/LMSAPIController.cs
@@ -0,0 +1,323 @@
+﻿using FHIR_LMS_WEBAPI_CORE.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace FHIR_LMS_WEBAPI_CORE.Controllers
+{
+    public class LMSAPIController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string fhirUrl;
+        private JObject loginData;
+        private HTTPrequest HTTPrequest = new HTTPrequest();
+        public LMSAPIController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            fhirUrl = _configuration.GetValue<string>("TZFHIR_Url");
+            loginData = JObject.Parse(System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Assets/JSON/LoginData.json")));
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        private void SendEmail(string Mail, string Subject, string Body)
+        {
+
+            MailMessage msg = new MailMessage();
+            //收件者，以逗號分隔不同收件者 ex "test@gmail.com,test2@gmail.com"
+            //msg.To.Add(string.Join(",", MailList.ToArray()));
+
+            msg.To.Add(Mail);
+            msg.From = new MailAddress("tcumih707@gmail.com", "TCUMI", System.Text.Encoding.UTF8);
+            //郵件標題 
+            msg.Subject = Subject;
+            //郵件標題編碼  
+            msg.SubjectEncoding = System.Text.Encoding.UTF8;
+            //郵件內容
+            msg.Body = Body;
+            msg.IsBodyHtml = true;
+            msg.BodyEncoding = System.Text.Encoding.UTF8;//郵件內容編碼 
+            msg.Priority = MailPriority.Normal;//郵件優先級 
+                                               //建立 SmtpClient 物件 並設定 Gmail的smtp主機及Port 
+            #region 其它 Host
+            /*
+             *  outlook.com smtp.live.com port:25
+             *  yahoo smtp.mail.yahoo.com.tw port:465
+            */
+            #endregion
+            SmtpClient MySmtp = new SmtpClient("smtp.gmail.com", 587);
+            //設定你的帳號密碼
+            MySmtp.Credentials = new System.Net.NetworkCredential("tcumih707@gmail.com", "hakoojgqwbosekwr");
+            //Gmial 的 smtp 使用 SSL
+            MySmtp.EnableSsl = true;
+            MySmtp.Send(msg);
+
+        }
+
+        [HttpPost("api/select-course")]
+        public IActionResult SelectCourse([FromBody] UserLogin user)
+        {
+            SelectCourse selectCourse = new SelectCourse(_configuration);
+
+            //var headers = Request.Headers;
+            //headers.TryGetValue("Authorization", out var headerValue);
+            //string token = headerValue.ToString();
+            string token = "123";
+            //if (token == null)
+            //{
+            //    return Unauthorized();
+            //}
+
+            // Get Person ID and Patient ID
+            if (user.personId == "" || user.patientId == "" || user.scheduleId == "")
+            {
+                return BadRequest("Patient ID or Person ID or Schedule ID not found.");
+            }
+
+            loginData["person"]["id"] = user.personId;
+            loginData["patient"]["id"] = user.patientId;
+
+            //loginData["schedule"]["id"] = "4534";
+            loginData["schedule"]["id"] = user.scheduleId;
+
+            //Check Login Data (Person == Patient)
+            loginData["errmsg"] = "Check Login Person failed.";
+            string param = '/' + loginData["person"]["id"].ToString();
+            JObject result = HTTPrequest.getResource(fhirUrl, "Person", param, token, selectCourse.GetUserRole, loginData); // result = a new FHIR Appointment
+            if (result["errmsg"] != null)
+            {
+                return BadRequest(result);
+            }
+
+            //Send Email
+
+            string FilePath = Directory.GetCurrentDirectory() + "\\Assets\\HTML\\ThankyouPage.html";
+            StreamReader str = new StreamReader(FilePath);
+            string MailText = str.ReadToEnd();
+            str.Close();
+
+            string mail = loginData["person"]["identifier"].ToString();
+            string subject = "感謝您報名 【"+ loginData["schedule"]["name"].ToString() + "】";
+
+            SendEmail(mail, subject, MailText);
+
+            result = HTTPrequest.getResource(fhirUrl, "Appointment", "?actor=Patient/" + user.patientId, token, null, loginData);   // return all appointments
+
+            return Ok(result);
+        }
+
+        [HttpPost("api/login")]
+        [Consumes("application/x-www-form-urlencoded")]
+        public IActionResult Login([FromForm] IFormCollection user)
+        {
+            LoginModel loginModel = new LoginModel(_configuration);
+
+            //Get Email
+            loginData["person"]["email"] = user["email"].ToString();
+
+            //Get Password
+            loginData["person"]["password"] = user["password"].ToString();
+
+            //Verify Email and Password
+            loginData["errmsg"] = "Check Login Person failed.";
+            string param = "?identifier=" + user["email"].ToString();
+            JObject result = HTTPrequest.getResource(fhirUrl, "Person", param, "", loginModel.Check, loginData);
+            if ((int)result["code"] != 200)
+            {
+                return Ok("Incorrect email or password.");
+            }
+
+            // Add roles to loginData
+            loginData.Remove("errmsg");
+            loginData["roles"] = result["roles"];
+
+            return Ok(loginData);
+            /* {
+                "roles": [
+                {
+                    "roleName": "Patient",
+                    "roleID": "46179774-1a1e-4ae4-b626-9ff53fa080f4",
+                    "practRoleID": "",
+                    "roleIdentifier": "",
+                    "organizationID": "98a6f5c5-af58-41b5-bd6f-d8ca996b5580",
+                    "organizationName": "Tzu Chi Hospital",
+                    "roleCode": [],
+                    "practID": ""
+                }],
+                "code": 200
+                } 
+            */
+        }
+
+
+        /// <summary>
+        /// user.["Person"] = Person JSON obj
+        /// user.["Patient"] = Patient JSON obj
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>{"patient": "Patient/[id]", "person": "Person/[id]}</returns>
+
+        [HttpPost("api/register")]
+        public IActionResult Register([FromBody] JObject user)
+        {
+            RegisterModel registerModel = new RegisterModel(_configuration);
+
+            JObject person = (JObject)user["Person"];
+            JObject patient = (JObject)user["Patient"];
+
+            dynamic res = new JObject();
+
+            //Search Person
+            //Verify Email and Password
+            loginData["errmsg"] = "Check Register Person failed.";
+            string param = "?identifier=" + person["identifier"][0]["value"].ToString();
+            JObject result = HTTPrequest.getResource(fhirUrl, "Person", param, "", null, loginData);
+            if ((result["code"] != null && (int)result["code"] == 404) || ((string)result["resourceType"] == "Bundle" && (string)result["type"] == "searchset" && result["entry"] == null)) //Person not found
+            {
+                // Generate UUID for Patient
+                Guid uuid = Guid.NewGuid();
+                string patientUUID = uuid.ToString();
+                patient["id"] = patientUUID;
+
+                // PUT Patient
+                JObject ret = HTTPrequest.putResource(fhirUrl, "Patient/" + patientUUID, patient, "", null, loginData);
+                //Post Patient
+                //JObject ret = HTTPrequest.postResource(fhirUrl, "Patient", patient, "", null, loginData);
+                if (ret["resourceType"] != null && ret["resourceType"].ToString() == "Patient")
+                {
+                    //Get Patient ID
+                    JArray patlink = JArray.Parse("[{\"target\": {\"reference\": \"Patient/" + ret["id"].ToString() + "\"}} ]");
+                    person.Add(new JProperty("link", patlink));
+
+                    //Store Patient ID
+                    res.patient = "Patient/" + ret["id"].ToString();
+
+                    // Generate UUID for Person
+                    uuid = Guid.NewGuid();
+                    string personUUID = uuid.ToString();
+                    person["id"] = personUUID;
+
+                    ret = HTTPrequest.putResource(fhirUrl, "Person/" + personUUID, person, "", null, loginData);
+
+                    if (ret["resourceType"] != null && ret["resourceType"].ToString() == "Person")
+                    {
+                        res.person = "Person/" + ret["id"].ToString();
+                        return Ok(res);
+                    }
+                }
+                else
+                {
+                    res.msg = "Register failed.";
+
+                    return Ok(res);
+                }
+            }
+            else
+            {
+                res.msg = "Account exists.";
+
+                return Ok(res);
+            }
+
+            return Ok(res);
+        }
+
+        [HttpPost("api/jwt")]
+        public IActionResult JWT([FromBody] JObject data)
+        {
+            //string d = Newtonsoft.Json.JsonConvert.SerializeObject(data["selectedDocRef"]);
+            dynamic docRef = data["selectedDocRef"];
+
+            string docUrl = (docRef["resource"]["content"] != null) ? docRef["resource"]["content"][0]["attachment"]["url"].ToString() : "";
+
+            List<string> endpoints = new List<string>();
+            endpoints.Add(docUrl);
+
+            // Audience (who or what the token is intended for)
+            int idx = docUrl.IndexOf("fhir/");
+            string _aud = docUrl[..idx];
+
+            string userRole = _aud + data["userSelectedRole"].ToString();
+            string author = (docRef["resource"]["author"] != null) ? docRef["resource"]["author"][0]["reference"].ToString() : "";
+
+
+            JWTModel jwtModel = new JWTModel(_configuration);
+            //Verify ID Token ("Authorization" Header)
+
+            //Check FHIR Consent (if author != oneself)
+            if (userRole != author)
+            {
+
+            }
+
+            // Generate Access Token for FHIR Document
+            string docToken = jwtModel.GenerateAccessToken(_aud, userRole, docUrl);
+
+            // Get FHIR Document
+            JObject result = HTTPrequest.getResource(docUrl, "", "", docToken, null, loginData);
+
+            if (result["message"] != null)
+            {
+                //Return to viewer URL and access token to client
+                return StatusCode(StatusCodes.Status500InternalServerError, result);
+            }
+            if (result["entry"] == null || result["entry"][0]["resource"] == null)
+            {
+                //Return to viewer URL and access token to client
+                return StatusCode(StatusCodes.Status500InternalServerError, "No document entry available.");
+            }
+
+            foreach (JObject entry in result["entry"])
+            {
+                if (entry["resource"]["resourceType"].ToString() == "Composition")
+                {
+                    JObject composition = (JObject)entry["resource"];
+                    foreach (JObject e in composition["section"][0]["entry"])
+                    {
+                        endpoints.Add(_aud + e["reference"].ToString());
+                    }
+                    break;
+                }
+            }
+
+            //Generate Access Token for FHIR Document & its content
+            string allToken = jwtModel.GenerateAccessToken(_aud, userRole, endpoints.ToArray());
+
+            JObject retData = new JObject();
+            retData["docURL"] = docUrl;
+
+            if (data["viewer"].ToString() == "skinlesion.report.document")
+            {
+                retData["viewerURL"] = "http://repo.tcumi.com:9876/viewer/skin-lesion-viewer";
+            }
+            else if (data["viewer"].ToString() == "skinlesion.image.document")
+            {
+                retData["viewerURL"] = "http://repo.tcumi.com:9876/skinlesionimage-ms/ReportCreator?documentbundle=" + docUrl;
+                //http://repo.tcumi.com:9876/ReportCreator?documentbundle=http://repo.tcumi.com:9876/fhir/Bundle/688
+            }
+            else
+            {
+                retData["viewerURL"] = "";
+                return BadRequest("Viewer not available.");
+            }
+
+            retData["accessToken"] = allToken;
+
+            //Return to viewer URL and access token to client
+            //string redirectUrl = "http://repo.tcumi.com:9876/viewer?wantedDoc=" + docUrl;
+            //return RedirectPermanent(redirectUrl);
+            return Ok(retData);
+        }
+    }
+}
---
+++ Validator/RoleController.cs
@@ -0,0 +1,49 @@
+﻿using FHIR_LMS_WEBAPI_CORE.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FHIR_LMS_WEBAPI_CORE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        private readonly string fhirUrl;
+        private JObject loginData;
+        private HTTPrequest HTTPrequest = new HTTPrequest();
+        public RoleController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            fhirUrl = _configuration.GetValue<string>("TZFHIR_Url");
+            loginData = JObject.Parse(System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Assets/JSON/LoginData.json")));
+        }
+
+        [HttpGet("mini-apps")]
+        public IActionResult MiniApps([FromQuery] string userRole)
+        {
+            // Get Role
+            
+            JObject apps=null;
+
+            if (userRole == "PractitionerRole")
+            {
+                apps = JObject.Parse(System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Assets/JSON/PractRole_Apps.json")));
+            }
+            else if (userRole == "Patient")
+            {
+                apps = JObject.Parse(System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Assets/JSON/Patient_Apps.json")));
+            }
+
+            return Ok(apps);
+        }
+    }
+}
